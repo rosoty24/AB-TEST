5396e3b920349421731cb8890d642556c1caa4a8
rosoty <[rosoty99@gmail.com]>
2016-05-21 12:14:26 +0700

    new

--- client/main.js
+++ client/main.js
@@ -20,3 +20,19 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+Template.original.rendered = function(){
+	//Google Analytics Content Experiment code 
+          function utmx_section(){}function utmx(){}(function(){var
+          k='121544381-9',d=document,l=d.location,c=d.cookie;
+          if(l.search.indexOf('utm_expid='+k)>0)return;
+          function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
+          indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
+          length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
+          '<sc'+'ript src="'+'http'+(l.protocol=='https:'?'s://ssl':
+          '://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
+          '&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
+          valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
+          '" type="text/javascript" charset="utf-8"><\/sc'+'ript>')})();
+          utmx('url','A/B');
+    // End of Google Analytics Content Experiment code 
+}
